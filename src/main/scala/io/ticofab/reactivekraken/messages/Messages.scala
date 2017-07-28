36564be9092403695079a558bce36de635a8ac2d
Fabio Tiriticco <fabio.europe@gmail.com>
2017-07-28 11:27:34 +0200

    added sealed traits to actor messages

--- src/main/scala/io/ticofab/reactivekraken/messages/Messages.scala
+++ src/main/scala/io/ticofab/reactivekraken/messages/Messages.scala
@@ -4,6 +4,8 @@ import io.ticofab.reactivekraken.model._
 
 sealed trait Message
 
+sealed trait MessageResponse
+
 case object GetCurrentAssets extends Message
 
 case class GetCurrentAssetPair(currency: String, respectToCurrency: String) extends Message
@@ -14,22 +16,22 @@ case object GetCurrentAccountBalance extends Message
 
 case class GetCurrentTradeBalance(asset: Option[String] = None) extends Message
 
-case object GetCurrentOpenOrders
+case object GetCurrentOpenOrders extends Message
 
-case object GetCurrentClosedOrders
+case object GetCurrentClosedOrders extends Message
 
-case class CurrentAssets(result: Either[List[String], Map[String, Asset]])
+case class CurrentAssets(result: Either[List[String], Map[String, Asset]]) extends MessageResponse
 
-case class CurrentAssetPair(result: Either[List[String], Map[String, AssetPair]])
+case class CurrentAssetPair(result: Either[List[String], Map[String, AssetPair]]) extends MessageResponse
 
-case class CurrentTicker(result: Either[List[String], Map[String, Ticker]])
+case class CurrentTicker(result: Either[List[String], Map[String, Ticker]]) extends MessageResponse
 
-case class CurrentAccountBalance(result: Either[List[String], Map[String, String]])
+case class CurrentAccountBalance(result: Either[List[String], Map[String, String]]) extends MessageResponse
 
-case class CurrentTradeBalance(result: Either[List[String], TradeBalance])
+case class CurrentTradeBalance(result: Either[List[String], TradeBalance]) extends MessageResponse
 
-abstract class OrderMessageResponse(result: Either[List[String], Map[String, Order]])
+abstract class OrderMessageResponse(result: Either[List[String], Map[String, Order]]) extends MessageResponse
 
-case class CurrentOpenOrders(result: Either[List[String], Map[String, Order]])
+case class CurrentOpenOrders(result: Either[List[String], Map[String, Order]]) extends MessageResponse
 
-case class CurrentClosedOrders(result: Either[List[String], Map[String, Order]])
+case class CurrentClosedOrders(result: Either[List[String], Map[String, Order]]) extends MessageResponse
